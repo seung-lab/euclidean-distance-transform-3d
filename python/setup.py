0871f805a92139dc2bc4a5d32ecf37773d76bbee
William Silversmith <william.silversmith@gmail.com>
2020-02-24 16:38:55 -0500

    install: get py35 to compile on MacOS

--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,16 @@
 import setuptools
+import sys
 
 import numpy as np
 
 # NOTE: If edt.cpp does not exist:
 # cython -3 --fast-fail -v --cplus edt.pyx
+
+extra_compile_args = [
+  '-std=c++11', '-O3', '-ffast-math', '-pthread'
+]
+if sys.platform == 'darwin':
+  extra_compile_args += [ '-stdlib=libc++', '-mmacosx-version-min=10.9' ]
 
 setuptools.setup(
   setup_requires=['pbr'],
@@ -16,10 +23,8 @@ setuptools.setup(
       'edt',
       sources=[ 'edt.cpp' ],
       language='c++',
-      include_dirs=[ np.get_include(), 'threadpool.h' ],
-      extra_compile_args=[
-        '-std=c++11', '-O3', '-ffast-math', '-pthread'
-      ]
+      include_dirs=[ np.get_include() ],
+      extra_compile_args=extra_compile_args,
     ),
   ],
   long_description_content_type='text/markdown',
